358255e2bc1fe6fd651c18b0ec0c29ede4e00f78
zsribna <zsribna@gmail.com>
2023-08-04 14:13:45 +0300

    update 3 test

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+isLocalStorage();
+
+form.addEventListener('input', throttle(onInput, 500));
+
+function onInput() {
+  const {
+    elements: { email, message },
+  } = form;
+
+  localStorage.setItem(
+    LOCALSTORAGE_KEY,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  evt.currentTarget.reset();
+}
+
+function isLocalStorage() {
+  if (!localStorage.getItem(LOCALSTORAGE_KEY)) {
+    return;
+  }
+  const { email, message } = form.elements;
+  email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email;
+  message.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message;
+}
