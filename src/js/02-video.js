aee0c47f67dd0ea4377cbee2ab09adc82bfe5b8a
zsribna <zsribna@gmail.com>
2023-08-04 14:05:50 +0300

    update video.js

---
+++ src/js/02-video.js
@@ -0,0 +1,20 @@
+import Player from '@vimeo/player';
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
