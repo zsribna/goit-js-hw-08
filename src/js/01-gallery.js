b0717f33fea6d40f56197e9993f1a8583726ff71
zsribna <zsribna@gmail.com>
2023-08-04 13:52:42 +0300

    test 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryBox = document.querySelector(".gallery");
+const imageMarkup = createGalleryBox(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", imageMarkup);
 
+function createGalleryBox(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`;
+    })
+    .join(" ");
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 console.log(galleryItems);
